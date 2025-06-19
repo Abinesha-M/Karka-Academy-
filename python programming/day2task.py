c7c2fe4071e4079013d2a737adf9ac8131756a3e
Abinesha-M <m.abineshaabinesha@gmail.com>
2025-06-19 16:33:43 +0530

    Add files via upload

---
+++ python programming/day2task.py
@@ -0,0 +1,72 @@
+#task 1
+# print("Hello word")
+
+#task 2
+# print("Abinesha")
+
+#task 3
+# print("Abinesha")
+# print(18)
+
+#task 4
+# int=100
+# str="Abinesha"
+# float=3.14
+# print(int)
+# print(str)
+# print(float)
+
+#task5
+# name="Abinesha"
+# age=19
+# print("My Name is:",name ,"My Age is",age)
+
+#task6
+# name="Abinesha"
+# age=20
+# print("Name:",name)
+# print("Age:",age)
+
+#task7
+# a=10
+# b=20
+# print("Addition:",a+b)
+# print("Subtraction:",a-b)
+# print("Multiplication:",a*b)
+# print("Division:",a/b)
+
+#task8
+# print("*")
+# print("**")
+# print("***")
+# print("****")
+
+#task9
+# name = "Abi"
+# age = 20
+# isStudent = True
+# print("Name:", name)
+# print("Age:", age)
+# print("Is a student?", isStudent)
+# name = "Abinesha"          
+# age += 1               
+# isStudent = not isStudent  
+# print("\nAfter modifying variables:")
+# print("Name:", name)
+# print("Age:", age)
+# print("Is a student?", isStudent)
+
+#task 10
+name="Abinesha"
+age=19
+dob="20-03-2005"
+height=11.5
+weight=45.8
+degree="B.Sc computer Science"
+gender="Female"
+print("Name:",name)
+print("Age:",age)
+print("Dob:",dob)
+print("Hight:",height)
+print("Weight:",weight)
+print("Gender:",gender)
