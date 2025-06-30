16f367230c665e5f77649f623a0ea28161c50f79
Abinesha-M <m.abineshaabinesha@gmail.com>
2025-06-30 16:20:13 +0530

    Add files via upload

---
+++ Task2/Abi.py
@@ -0,0 +1,134 @@
+# #task1
+# teacherName="Abinesha"
+# teacherSalary="25000"
+# idNo=18
+# teacher_dept="Maths"
+# teacher_email="abi12342@gmai.com"
+# teacher_mobileno="1234567819"
+# print("Enter  teacher Name:",teacherName)
+# print("Enter  teacher Salary:",teacherSalary)
+# print("Enter  teacher idno:",idNo)
+
+# print("Enter  teacher Department:",teacher_dept)
+# print("Enter  teacher Email id:",teacher_email)
+# print("Enter  teacher mobile no:",teacher_mobileno)
+
+#task2
+# a=50
+# b=80
+# c=a+b
+# print("The Sum :",a+b)
+
+#task3
+# r = 5
+# pi = 3.14
+# area = pi * r * r
+# print("Area of circle is:", area)
+
+# #task4
+# length=float(input("Enter the length:"))
+# width=float(input("Enter the width:"))
+# area=length * width
+# print("Area  rectangle is:",area)
+
+# #task5
+# base=float(input("Enter the base of triangle:"))
+# height=float(input("Enter the height of triangle:"))
+# area= (base * height) / 2. 
+# print("Area of triangle is:",area)
+
+
+#task6
+# a=float(input("Enter a value :"))
+# b=float(input("Enter b value :"))
+# print("Addition:",a+b)
+# print("subtraction:",a-b)
+# print("multiplication:",a*b)
+# print("division:",a/b)
+
+# task7
+# a=10
+# print("Assignment operator:",a)
+# a+=2
+# print("Addition Assignment  operator:",a)
+# a-=5
+# print("subtraction Assignment operator:",a)
+# a*=5
+# print("Multiplication Assignment operator",a)
+# a/=5
+# print("Division Assignment operator",a)
+
+#task8
+# increment=5
+# decrement=5
+# print("Increment:",increment)
+# print("Decrement:",decrement)
+# print("After modify")
+# increment+=2
+# decrement-=4
+# print("Increment:",increment)
+# print("Decrement:",decrement)
+
+#task9
+# a=30
+# b=50
+# a==b
+# print("a==b:",a==b)
+# print("a!=b:",a!=b)
+# print("a>b:",a>b)
+# print("a<b:", a<b)
+# print("a>=b:", a>=b)
+# print("a<=b:",a<=b)
+
+# task10
+# a=True
+# b=False
+# print("a and b:",a and b)
+# print("a or b:",a or b)
+# print("not a:",not a)
+# print("not b:",not b)
+
+#task11
+# a=10
+# b=20
+# temp=a
+# a=b
+# b=temp
+# print("Swapping Using Third Variable")
+# print("a=",a)
+# print("b=",b)
+
+# x=30
+# y=40
+# x,y=y,x
+# print("Swapping Without Third Variable")
+# print("X=",x)
+# print("Y=",y)
+
+#task12
+# a=int(input("Enter a value:"))
+# b=int(input("Ente b value:"))
+# c=int(input("Enter c value:"))
+# average=(a+b+c)/3
+# print("Average:",average)
+
+
+#task13
+# a=10
+# b=20
+# c=12
+# d=3
+# H=((a+b)*c)/d
+# print("Compound Arithmetic operation:",H)
+
+#task14
+tamil=99
+eng=100
+maths=80
+science=98
+social=96
+total_marks =tamil+eng+maths+science+social
+avg=total_marks/5
+print("total_marks:",total_marks)
+print("Average:",avg,)
+
