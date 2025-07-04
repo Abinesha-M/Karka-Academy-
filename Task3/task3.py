60ea5c1f1c6f717856ee00f28b66d6f740d78e6b
Abinesha-M <m.abineshaabinesha@gmail.com>
2025-07-04 22:24:17 +0530

    Add files via upload

---
+++ Task3/task3.py
@@ -0,0 +1,51 @@
+#task1
+
+# def sayHello(name):
+#     print("Hello,"+ name + "!")
+# sayHello("Abinesha") 
+
+#task2
+
+# def add(a,b):
+#     return a+b
+# print("Sum1:", add(12,15))
+# print("Sum2:", add(22,25))
+# print("Sum3:", add(32,35))
+# print("Sum4:", add(42,45))
+
+#task3
+# def multiply(x,y):
+#     return x*y
+# print("product1:",multiply(12,15))
+# print("product2:",multiply(20,25))
+# print("product3:",multiply(13,11))
+
+#task4
+def multiply(a,b):
+     return a*b
+result=multiply(15,12)
+print("Product:",result)
+
+
+#task5
+# def divide(a,b):
+#     return a/b
+# output=divide(14,16)
+# print("Division:",output)   
+
+#task6
+# def factorial(n):
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#     return result
+# print(factorial(5))  
+# print(factorial(0))  
+# print(factorial(3))  
+ 
+#task7
+def square(number):
+    return number*number
+print("Square1:",square((4)))
+print("Square2:",square(-5))
+print("square3:",square(0))
